12ce4833fc57cb81e8829acd939e7442d44c5484
MatthewDurkin <35670435+MatthewDurkin@users.noreply.github.com>
2021-02-17 12:53:49 -0500

    Changes

--- poem-generator.php
+++ poem-generator.php
@@ -4,20 +4,20 @@
         <h1> Here's Your Valentine's Poem </h1> 
         <?php
 
-        $submit = filter_input(INPUT_POST, 'submit') 
+        $submit = filter_input(INPUT_POST, 'submit');
 
         function poemDisplay() {
             $colour = filter_input(INPUT_POST, 'colour');
             $noun = filter_input(INPUT_POST, 'noun');
             $person = filter_input(INPUT_POST, 'person');
-            echo "<div class="poemDiv">";
+            echo "<div class='poemDiv'>";
             echo "<p> Roses are $colour.</p>";
-            echo "<p> $Noun are blue. </p>";
+            echo "<p> $noun are blue. </p>";
             echo "<p> Dear $person, Happy Valentine's Day to you! </p>";
             echo "</div>"; 
         }
         // 5. logic error - no ! 
-        if(!isset($submit)) {
+        if(isset($submit)) {
             poemDisplay(); 
         }
         else {
